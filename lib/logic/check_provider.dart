1aad816dfb68f4e2ab55f311061eb9fb3a04ff3c
Constantijn Baarendse <cbaarendse@live.nl>
2020-05-09 09:15:32 +0200

    Initial - setup app with flutter; Built logic into check_provider.dart;

---
+++ lib/logic/check_provider.dart
@@ -0,0 +1,47 @@
+import "dart:collection";
+import "package:flutter/foundation.dart";
+import "package:flutter/material.dart";
+
+class CheckProvider with ChangeNotifier {
+  // private properties
+  static Duration _testMinute = Duration(
+      hours: 0, minutes: 1, seconds: 2, milliseconds: 250, microseconds: 0);
+  static var _startTime = DateTime.now();
+  var _stopTime = _startTime.add(_testMinute);
+
+  Map<String, int> _variations;
+
+  //Private methods
+  void _calculateVariation() {
+    //Stoptime - starttime
+    Duration standardMinute = Duration(minutes: 1);
+    Duration measuredMinute = _stopTime.difference(_startTime);
+    Duration variation = measuredMinute - standardMinute;
+
+    _variations['oneDay'] = variation.inMilliseconds * Duration.minutesPerDay;
+    _variations['sevenDays'] =
+        variation.inMilliseconds * Duration.minutesPerDay * 7;
+    _variations['thirtyDays'] =
+        variation.inMilliseconds * Duration.minutesPerDay * 30;
+    _variations['threehundredsixtyfiveDays'] =
+        variation.inMilliseconds * Duration.minutesPerDay * 365;
+  }
+
+  // Public methods
+  int millisecondsToMinutes(d) {
+    return d / Duration.millisecondsPerMinute;
+  }
+
+  int millisecondsToHours(d) {
+    return d / Duration.millisecondsPerHour;
+  }
+
+  int millisecondsToDays(d) {
+    return d / Duration.millisecondsPerDay;
+  }
+
+// Getters
+  Map<String, int> get variations {
+    return variations;
+  }
+}
