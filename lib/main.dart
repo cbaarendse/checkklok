95606fb72f23caafdd1a0d0336603a8082e7348d
Constantijn Baarendse <cbaarendse@live.nl>
2020-05-12 17:15:55 +0200

    Set up Drawer; Implemented screenshot button; Worked on layout.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'dart:io';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:checkklok/logic/check_provider.dart';
 import 'package:checkklok/screens/ad_banner.dart';
 import 'constants.dart';
 import 'package:provider/provider.dart';
 import 'package:admob_flutter/admob_flutter.dart';
+import 'package:native_screenshot/native_screenshot.dart';
 
 void main() {
   String getBannerAdUnitId() {
@@ -58,26 +60,62 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: IconButton(icon: Icon(Icons.menu), onPressed: () {}),
         title: Text(widget.title),
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.color_lens),
             onPressed: () {},
           ),
+          IconButton(
+            icon: Icon(Icons.mobile_screen_share),
+            onPressed: () async {
+              String path = await NativeScreenshot.takeScreenshot();
+              return;
+            },
+          ),
         ],
       ),
+      drawer: Drawer(
+          child: ListView(
+        padding: EdgeInsets.all(0.0),
+        children: <Widget>[
+          DrawerHeader(
+            decoration:
+                BoxDecoration(color: Theme.of(context).secondaryHeaderColor),
+            child:
+                Text('CheckKlok', style: Theme.of(context).textTheme.subtitle1),
+          ),
+          ListTile(
+            leading: Icon(Icons.delete_sweep),
+            title: Text('Delete unused images',
+                style: Theme.of(context).textTheme.subtitle2),
+          ),
+          ListTile(
+            leading: Icon(Icons.lightbulb_outline),
+            title:
+                Text('How to..', style: Theme.of(context).textTheme.subtitle2),
+          ),
+          ListTile(
+            leading: Icon(Icons.settings),
+            title:
+                Text('Settings', style: Theme.of(context).textTheme.subtitle2),
+          ),
+        ],
+      )),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             //AdBanner(),
             Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: <Widget>[
                 Card(
+                  margin: EdgeInsets.only(left: 12.0),
                   child: Column(
                     children: <Widget>[
+                      Text('Start',
+                          style: Theme.of(context).textTheme.headline6),
                       Image.asset(
                         'images/IMG_8294.jpeg',
                         width: 120.0,
@@ -98,8 +136,11 @@ class _MyHomePageState extends State<MyHomePage> {
                   ),
                 ),
                 Card(
+                  margin: EdgeInsets.only(right: 12.0),
                   child: Column(
                     children: <Widget>[
+                      Text('Stop',
+                          style: Theme.of(context).textTheme.headline6),
                       Image.asset(
                         'images/IMG_8295.jpeg',
                         width: 120.0,
@@ -121,38 +162,57 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
               ],
             ),
-            FlatButton(
-              child: Text('Check your clock'),
-              onPressed: () {},
+            Row(
+              mainAxisSize: MainAxisSize.max,
+              children: <Widget>[
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.symmetric(horizontal: 12.0),
+                    child: RaisedButton(
+                      color: Theme.of(context).buttonColor,
+                      splashColor: Theme.of(context).accentColor,
+                      hoverColor: Theme.of(context).hoverColor,
+                      child: Text('Check your clock'),
+                      onPressed: () {},
+                    ),
+                  ),
+                ),
+              ],
             ),
             Expanded(
               child: ListView(
-                padding: EdgeInsets.all(12.0),
+                padding: EdgeInsets.symmetric(horizontal: 12.0),
                 children: <Widget>[
                   ListTile(
-                    leading: Icon(Icons.watch_later),
+                    leading: Icon(Icons.timer),
                     title: Text('Per minute'),
-                    subtitle: Text('This watch is 1 second off'),
+//                    subtitle: Text('This watch is 1 second off'),
                     trailing: Icon(Icons.arrow_downward),
                   ),
                   ListTile(
                     leading: Icon(Icons.watch_later),
                     title: Text('Per day'),
-                    subtitle: Text('This watch is 1440 seconds off'),
+//                    subtitle: Text('This watch is 1440 seconds off'),
                     trailing: Icon(Icons.arrow_downward),
                   ),
                   ListTile(
-                    leading: Icon(Icons.watch_later),
+                    leading: Icon(Icons.event),
                     title: Text('Per 7 days'),
-                    subtitle: Text('This watch is 10.080 seconds off'),
+//                    subtitle: Text('This watch is 10.080 seconds off'),
                     trailing: Icon(Icons.arrow_downward),
                   ),
                   ListTile(
-                    leading: Icon(Icons.watch_later),
+                    leading: Icon(Icons.event_note),
                     title: Text('Per 30 days'),
-                    subtitle: Text('This watch is 302.400 seconds off'),
+//                    subtitle: Text('This watch is 302.400 seconds off'),
                     trailing: Icon(Icons.arrow_downward),
-                  )
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.calendar_today),
+                    title: Text('Per 365 days'),
+//                    subtitle: Text('This watch is 302.400 seconds off'),
+                    trailing: Icon(Icons.arrow_downward),
+                  ),
                 ],
               ),
             ),
